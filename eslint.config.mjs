b83b104c2f3fe35528070d91fa152550cf8a1fe3
Hyun Oh <hyunnoh01@gmail.com>
2025-12-25 21:17:22 +0900

    ci: resolve eslint warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,37 @@
 // @ts-check
 
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import globals from "globals";
 
 export default defineConfig([
+  globalIgnores(["**/dist"]),
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
     files: ["packages/ui/**/*.{js,jsx,ts,tsx}"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     extends: [
       reactPlugin.configs.flat.recommended,
       reactPlugin.configs.flat["jsx-runtime"],
       reactHooks.configs.flat.recommended,
     ],
+  },
+  {
+    files: ["packages/ui/vite.config.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
   },
   eslintConfigPrettier,
 ]);
